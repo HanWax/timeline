c62943c8032b8726f915ca44752a51f3a1498d72
Hannah Waxman <hannah.waxman8@gmail.com>
2014-09-07 12:23:10 +0100

    Preparing for running rspec

--- spec/support/omniauth.rb
+++
@@ -1,9 +0,0 @@
-OmniAuth.config.test_mode = true
-OmniAuth.config.mock_auth[:moves] = OmniAuth::AuthHash.new({
-															 :provider => ‘moves’,
-															 :uid => ‘123456789’,
-															 :user_info => {
-																 :user_id => 1
-															 }
-															 :credentials => {:token => ‘12345’}
-})
